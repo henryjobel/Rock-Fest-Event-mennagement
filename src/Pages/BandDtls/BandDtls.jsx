170afb085e7ac77386a8290f4d0c0bec25fb7536
henryjobel <jhobelsaha@gmail.com>
2023-10-08 06:56:15 +0600

    band details created and styles also done wtmrw i will intrgate the fire base

--- src/Pages/BandDtls/BandDtls.jsx
+++ src/Pages/BandDtls/BandDtls.jsx
@@ -11,12 +11,26 @@ const BandDtls = () => {
         return <div>Band not found</div>;
     }
 
+    const backgroundImageStyle = {
+        backgroundImage: `url(${selectedBand.dtlsImg})`,
+    };
+
     return (
-        <div className='text-red-600'>
-            <h2>{selectedBand.title}</h2>
-            <img src={selectedBand.imge} alt="Band Details" />
-            <p>{selectedBand.longDescription}</p>
-            <p>Hire Price: {selectedBand.hirePrice}</p>
+        <div>
+            <div className="hero min-h-screen" style={backgroundImageStyle}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero min-h-screen">
+                    <div className="hero-content flex-col lg:flex-row">
+                        <img src={selectedBand.imge} className="max-w-sm rounded-lg shadow-2xl" />
+                        <div>
+                            <h1 className="text-5xl font-bold text-red-500">{selectedBand.title}</h1>
+                            <p className="py-6 text-white text-3xl font-semibold">{selectedBand.shortDescription}</p>
+                            <p className="py-4 text-white text-xl font-medium">{selectedBand.longDescription}</p>
+                            <button className="btn btn-primary">Get Started</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
